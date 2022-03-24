d610d324e3706cf575963b4211ea7bf61103187e
Jeff Schulz <jeffschulz7@gmail.com>
2022-03-24 00:01:11 +0000

    bad version of post w/ iframe. moving to file upload instead

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+// Requires
 const http = require('http');
 // const url = require('url'); //parse url params
 const fs = require('fs'); //file system
 const mysql = require('mysql');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const sl = require('./scrape_learfield');
 const { resourceLimits } = require('worker_threads');
 const { join } = require('path');
 
+// Parameters
 const hostname = '0.0.0.0';
 const port = 3030;
 
+// MySQL
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -32,7 +38,7 @@ con.query("SELECT * FROM camnotes.Teams where id=3;", function (err, result, fie
 });
 
 
-
+// HTML
 default_headshots_html = `
   <html>
   <style>
@@ -55,61 +61,94 @@ default_headshots_html = `
 `;
 
 
+// Express Server
+const app = express();
+app.use(cors({origin: '*'}));
+// app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '150mb'}));
 
-const server = http.createServer((req, res) => {
-  // main html
-  
-  
-  
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  // if(!req.rawHeaders.includes('Referer')) {
-    
-  console.log('Request to '+req.url)
+ 
 
-
-  if(req.url == '/') {
-    fs.writeFile('./headshots.html', default_headshots_html, () => {
-      console.log('Done wiping headshots.html')
-    });
-
-    fs.readFile('./index.html', function(err, data) {
-      var sl_obj = new sl.Scrape_Learfield(done_scraping);
-      var players = sl_obj.scrape(url_base, sport);
-
-      res.write(data);
-      res.end();
-    });
-
-
-
-  } else if(req.url == '/headshots') {
-    var subpage = req.url.slice(1,req.url.length)
-    var text = "Loading game " + subpage + "...";
-    console.log('doing headshots?')
-    fs.readFile('./headshots.html', function(err, data) {
-      res.write(data);
-      res.end();
-    });
-
-
-
-  } else {
-    console.log('ignored request')
+// Display printable headshots
+app.get('/headshots', function(req, res) {
+  var subpage = req.url.slice(1,req.url.length)
+  var text = "Loading game " + subpage + "...";
+  console.log('doing headshots?')
+  fs.readFile('./headshots.html', function(err, data) {
+    res.write(data);
     res.end();
-  }
-
+  });
 });
 
 
+// FULL data re-grab
+app.get('/redo_data', function (req, res) {
+  fs.writeFile('./headshots.html', default_headshots_html, () => {
+    console.log('Done wiping headshots.html')
+  });
 
+  fs.readFile('./index.html', function(err, data) {
+    var sl_obj = new sl.Scrape_Learfield(done_scraping);
+    var players = sl_obj.scrape(url_base, sport);
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    res.write(data);
+    res.end();
+  })
 });
 
 
+// FULL data re-grab
+app.post('/send_iframe', function (req, res) {
+  console.log('RECEIVED POST REQ')
+  console.log(req.body);
+  console.log(req.params);
+  // fs.writeFile('./roster_test.html', req, () => {
+  //   console.log('Done wiping headshots.html')
+  // });
+  res.end();
+});
 
+
+// FULL data re-grab
+app.get('/admin', function (req, res) {
+  text1 = `<script type="text/javascript" src="https://code.jquery.com/jquery-1.7.1.min.js"></script>
+    <script>function send_iframe_data(){ 
+    console.log("doing the ajax");
+    var html_text = $('#learfield_window')[0];
+    var empty;
+    html_text.contentWindow.postMessage(empty, '*');
+    var payload = html_text.contentWindow.document.slice(0,40);
+    // console.log(html_text.contentWindow.document);
+    $.ajax({
+      type: 'POST',
+      url: 'http://cams.schulzvideo.com/send_iframe',
+      dataType: "json",
+      data: '{"test_data":"'+payload+'"}',
+      contentType: "application/json"
+    });
+    //$.post('http://cams.schulzvideo.com/send_iframe', {test_data:"test_value"});
+  }</script>`
+  text2 = '<button class="btn" onclick="send_iframe_data()">SEND IFRAME DATA</button><iframe id="learfield_window" style="width:100%; height:100%" src="https://gozags.com/sports/baseball/roster"/>'
+
+  res.end(text1+text2);
+
+  // fs.readFile('./index.html', function(err, data) {
+  //   var sl_obj = new sl.Scrape_Learfield(done_scraping);
+  //   var players = sl_obj.scrape(url_base, sport);
+
+  //   res.write(data);
+  //   res.end();
+  // })
+});
+
+
+// Express listen
+app.listen(port, function() {
+  console.log(`Example app listening on port ${port}!`)
+});
+
+
+// Build headshots html file
 function done_scraping(player_urls, player_list) {
   full_html = '<div class="grid-container">\n';
   for(let i=0; i<player_urls.length; i++) {
